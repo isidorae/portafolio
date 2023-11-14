3177f94bfbc178c5b1ea042e76e5f68f7bab7844
isidorae <mariaisidoraerrazuriz@gmail.com>
2023-11-14 15:36:05 -0300

    portafolio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function openGithub() {
+    window.open("https://github.com/isidorae/", "_blank")
+}
+
+function openLinkedin() {
+    window.open("https://www.linkedin.com/in/isidora-errazuriz-a9267412b/", "_blank")
+}
+
+function downloadCV() {
+    window.open("isidora_errazurizCV2023.pdf", '_blank');
+}
